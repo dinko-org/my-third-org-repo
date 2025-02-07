cbf78879d55e1d757f1496b5c715ddd5eca47961
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-07 15:49:57 +0100

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "myvnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
-  location            = module.resource-group.location
-  resource_group_name = module.resource-group.name
+  location            = outputs.rg.location
+  resource_group_name = outputs.rg.name
 }
 
 resource "azurerm_subnet" "mysubnet" {
   name                 = var.subnet_name
-  resource_group_name  = module.resource-group.name
+  resource_group_name  = outputs.rg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = var.subnet_address_prefixes
 }
