758db66552ac2adf1f861ff5799ae778d88054d6
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-07 14:57:05 +0100

    Create main.tf for vnet and subnet

---
+++ modules/network/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_virtual_network" "myvnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = module.resource-group.location
+  resource_group_name = module.resource-group.name
+}
+
+resource "azurerm_subnet" "mysubnet" {
+  name                 = var.subnet_name
+  resource_group_name  = module.resource-group.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = var.subnet_address_prefixes
