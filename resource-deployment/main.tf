dcdca4d12f941409d3ce1acce31ba4aa0192233e
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-07 15:58:04 +0100

    Update main.tf

--- resource-deployment/main.tf
+++ resource-deployment/main.tf
@@ -15,6 +15,8 @@ module "storage" {
 
 module "network" {
   source                   = "../modules/network"
+  resource_group_name      = module.resource-group.name
+  location                 = var.location
   vnet_name                = var.vnet_name
   vnet_address_space       = var.vnet_address_space
   subnet_name              = var.subnet_name
