c5c654c768f53e92218528eaaad9f7fc7255063e
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-12 12:28:22 +0100

    Update main.tf with KV module

--- resource-deployment/main.tf
+++ resource-deployment/main.tf
@@ -21,5 +21,15 @@ module "network" {
   vnet_address_space       = var.vnet_address_space
   subnet_name              = var.subnet_name
   subnet_address_prefixes  = var.subnet_address_prefixes
+}
 
+module "key-vault" 
+{
+  source   = "../modules/key-vault"
+  name     = var.kv_name
+  location = var.location
+  resource_group_name      = module.resource-group.name
+  sku = var.sku_name
+  secret_permissions = var.secret_permissions
+  storage_permissions = var.storage_permissions
 }
