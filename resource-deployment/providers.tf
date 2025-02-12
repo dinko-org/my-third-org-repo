d8f11b93a047ed2d8128094ff307c28167fdb29b
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-12 14:59:18 +0100

    Update providers.tf

--- resource-deployment/providers.tf
+++ resource-deployment/providers.tf
@@ -17,6 +17,11 @@ terraform {
   }
 }
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
   use_oidc = true
 }
