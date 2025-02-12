9538d0cc83df9ab53325a6ed8295d4195b8b6e64
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-12 14:43:56 +0100

    changing kv name object

--- modules/key-vault/main.tf
+++ modules/key-vault/main.tf
@@ -1,6 +1,6 @@
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "dinkokv" {
+resource "azurerm_key_vault" "dinkokvtf" {
   name                = var.kv_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "dinkokv" {
 }
 
 resource "azurerm_key_vault_access_policy" "dinkokvap" {
-  key_vault_id = azurerm_key_vault.dinkokv.id
+  key_vault_id = azurerm_key_vault.dinkokvtf.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
 
   # The object_id of the Managed Identity for the VM
@@ -30,11 +30,11 @@ resource "random_password" "windows_admin" {
 resource "azurerm_key_vault_secret" "windows_password" {
   name         = "windows-vm-password"
   value        = random_password.windows_admin.result
-  key_vault_id = azurerm_key_vault.dinkokv.id
+  key_vault_id = azurerm_key_vault.dinkokvtf.id
 }
 
 resource "azurerm_role_assignment" "keyvault_secrets_access" {
-  scope                = azurerm_key_vault.dinkokv.id
+  scope                = azurerm_key_vault.dinkokvtf.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = data.azurerm_client_config.current.object_id
 }
