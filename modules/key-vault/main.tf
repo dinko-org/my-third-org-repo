f4e245f6e70d5508f5981c3aa2b02d1f26a31c12
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-12 11:40:13 +0100

    Create main.tf for KeyVault resource

---
+++ modules/key-vault/main.tf
@@ -0,0 +1,23 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "dinkokv" {
+  name                = var.kv_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = var.sku_name
+
+}
+
+resource "azurerm_key_vault_access_policy" "dinkokvap" {
+  key_vault_id = azurerm_key_vault.dinkokv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+
+  # The object_id of the Managed Identity for the VM
+  object_id = data.azurerm_client_config.current.object_id
+
+  secret_permissions = var.secret_permissions
+  
+  storage_permissions = var.storage_permissions
+
+}
